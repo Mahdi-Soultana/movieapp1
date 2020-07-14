fb5c37f6579c0efeaefcfb48743fb39538b27274
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2020-07-14 09:09:34 -0700

    h1 +P

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1>Hello mahdi from GitHub congratulation !!!</h1>, document.getElementById('root')
+const card =(<div>
+  
+    <h1>Hi Mahdi Soultana </h1>
+     <p>You are the best just continue</p>
+
+</div>)
+
+ReactDOM.render(card, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
