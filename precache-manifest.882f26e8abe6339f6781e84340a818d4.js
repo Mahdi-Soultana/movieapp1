fea1fe36c760031b70dd8ba7570c89f1bc317f2f
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2020-07-14 08:56:38 -0700

    Updates

---
+++ precache-manifest.882f26e8abe6339f6781e84340a818d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422af8be867f4541be06d1df856ec049",
+    "url": "/movieapp1/index.html"
+  },
+  {
+    "revision": "4f3d7c18e7364496cb3b",
+    "url": "/movieapp1/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "47f238e8e703baaf203f",
+    "url": "/movieapp1/static/js/2.a1f503db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movieapp1/static/js/2.a1f503db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f3d7c18e7364496cb3b",
+    "url": "/movieapp1/static/js/main.6a6e431f.chunk.js"
+  },
+  {
+    "revision": "f37c2975413359d82375",
+    "url": "/movieapp1/static/js/runtime-main.c9905cbb.js"
+  }
+]);
